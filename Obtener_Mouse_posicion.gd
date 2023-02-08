4f9d759c88add07b3c7a88a827b440668d6c373d
faus1000 <operyde@gmail.com>
2023-02-08 05:29:21 -0400

    Create Obtener_Mouse_posicion.gd

---
+++ Obtener_Mouse_posicion.gd
@@ -0,0 +1,9 @@
+func _input(event):
+   # Mouse in viewport coordinates.
+   if event is InputEventMouseButton:
+       print("Mouse Click/Unclick at: ", event.position)
+   elif event is InputEventMouseMotion:
+       print("Mouse Motion at: ", event.position)
+
+   # Print the size of the viewport.
+   print("Viewport Resolution is: ", get_viewport_rect().size)
